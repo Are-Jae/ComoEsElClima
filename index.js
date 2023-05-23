18c7ea9df84cba1e647bf78241ea10e573a960cc
Are-Jae <evansraven7@gmail.com>
2023-05-22 22:30:13 -0400

    broken code, needs debugged

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const weatherAPI= "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={367189446bf220ed3e94d1f7f746deec}";
-const city = document.querySelector("#locationSearch").value 
+const city = document.getElementById("locationSearch").value 
 const searchBtn = document.querySelector("#search");
 const APIkey = "367189446bf220ed3e94d1f7f746deec";
 const forecast = document.querySelectorAll("#forecast");
@@ -7,24 +7,38 @@ const forecast = document.querySelectorAll("#forecast");
 
 
 
+//add lat, lon, appid var 
+//find lat &  lon for city
+//use lat lon funcx to grab weather info for city 
 
 
 
 
+
+//  funcx calling API
 function Weeklyweather() {
+    console.log(city)
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        .then((data) => { console.log(data)}) 
+        
+        // .then((data) => {
+        //     console.log(data);
+        // })
+        // .catch((err) => {
+        //     console.log(err);
+        // });
 }
 
 searchBtn.addEventListener('click', Weeklyweather);
 
-//  funcx calling API
+
+
+
+
+
+
+
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // funcx calling api data to display on DOM
 // funcx to save session storage 
 //need to save session storage 

95ec7348778fb9daa732a49e0a257e2a8afae4b2
Are-Jae <evansraven7@gmail.com>
2023-05-24 09:06:06 -0400

    added notes from debugging, implement in my own way before submitting hw

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
-const weatherAPI= "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={367189446bf220ed3e94d1f7f746deec}";
-const city = document.getElementById("locationSearch").value 
+// const weatherAPI= "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={367189446bf220ed3e94d1f7f746deec}";
+// const city = document.getElementById("locationSearch").value 
+// const searchBtn = document.querySelector("#search");
+// const APIkey = "367189446bf220ed3e94d1f7f746deec";
+// const forecast = document.querySelectorAll("#forecast");
+
+
+// const errorMessage = "Unable to find city, try again?"
+
+
+
+
+// const search = document.getElementById("search");
+// search.onclick = function(event){
+//     console.log(event, "is working")
+// }
+
+
+// //  funcx calling API
+// function Weeklyweather() {
+//     if ( search === 'clicked') {
+//         fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`);//get correct url 
+//         console.log("Is working");
+        
+//     } else {
+//        return (errorMessage) 
+//     }
+   
+        
+// }
+
+
+
+// Weeklyweather(); 
+
+
+
+const weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={367189446bf220ed3e94d1f7f746deec}";
+const cityInput = document.getElementById("locationSearch");
 const searchBtn = document.querySelector("#search");
 const APIkey = "367189446bf220ed3e94d1f7f746deec";
 const forecast = document.querySelectorAll("#forecast");
+const errorMessage = "Unable to find city, try again?"; //needs to display on dom or return? 
+
+searchBtn.addEventListener("click", function() {
+  const city = cityInput.value;
+  if (city !== '') {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`)
+      .then(response => response.json())
+      .then(data => {
+        // Process the received data
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(errorMessage, error);
+      });
+  } else {
+    console.log(errorMessage);
+  }
+});
 
 
-const errorMessage = "Unable to find city, try again?"
+
 
 //add lat, lon, appid var 
 //find lat &  lon for city
 //use lat lon funcx to grab weather info for city 
-
-
-const search = document.getElementById("search");
-search.onclick = function(event){
-    console.log(event)
-}
-
-
-//  funcx calling API
-function Weeklyweather() {
-    if ( search === 'clicked') {
-        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`)
-        
-    } else {
-       return (errorMessage) 
-    }
-   
-        
-}
-
-
-
-
-
-
-
-
 
 
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -60,6 +88,6 @@ function Weeklyweather() {
     
 
 
-    Weeklyweather(); 
+    
 
    
